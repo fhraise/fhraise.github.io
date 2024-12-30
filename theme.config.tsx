5d198431b76c8386267f568beb23157bdb6886ad
LTFan <xfqwdsj@qq.com>
2024-12-30 20:59:10 +0800

    fix: footer

--- theme.config.tsx
+++ theme.config.tsx
@@ -8,13 +8,13 @@ const config: DocsThemeConfig = {
       titleTemplate: "%s – Fhraise",
     };
   }, head: <>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta property="og:title" content="Fhraise"/>
-    <meta property="og:description" content="HSAS Foodies"/>
-  </>, logo: <FhraiseLogo/>, project: {
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:title" content="Fhraise" />
+    <meta property="og:description" content="HSAS Foodies" />
+  </>, logo: <FhraiseLogo />, project: {
     link: "https://github.com/fhraise/Fhraise",
   }, docsRepositoryBase: "https://github.com/fhraise/fhraise.github.io", footer: {
-    text: `© ${new Date().getFullYear()} HSAS Foodies.`,
+    content: (<span>© ${new Date().getFullYear()} HSAS Foodies.</span>),
   }, toc: { backToTop: true },
 };
 
