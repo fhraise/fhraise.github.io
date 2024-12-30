64da69f468c3159f59bd04fb14ce8edc7a535434
LTFan <xfqwdsj@qq.com>
2024-12-30 20:40:38 +0800

    refactor: fix module error (#23)

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const withNextra = require("nextra")({
-  theme: "nextra-theme-docs", themeConfig: "./theme.config.tsx",
-});
-
-module.exports = withNextra({
-  i18n: {
-    locales: ["zh-CN"], defaultLocale: "zh-CN",
-  },
-});
